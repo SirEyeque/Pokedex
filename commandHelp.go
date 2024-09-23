4b801a9bf72be44cc47f42f0f3374c47d9771867
SirEyeque <sireyeque@gmail.com>
2024-09-23 17:13:39 -0600

    Added comments and cleaned up the code. Same functionality as prev commit

--- commandHelp.go
+++ commandHelp.go
@@ -5,13 +5,23 @@ import(
 )
 
 
+/*
+*	function:	commandHelp()
+*
+*	output:		error
+*
+*	purpose:	Prints a statement to the terminal on how to use
+*				the Pokedex commands
+*/
 func commandHelp() error{
 	note := `
-Enter in any of the following commands to the Pokedex commandline
 
-Exit
-Help
+Enter in any of the following commands to the Pokedex commandline.
+Commands are NOT case sensitive.
 
+Help: Prints a help message
+
+Exit: Exits Pokedex and returns you to the terminal
 `
 	fmt.Print(note)
 	return nil
